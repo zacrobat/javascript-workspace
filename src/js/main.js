40e52e3aee65a73bd208a62925f937b960588db7
Zac Cobb <zacrobat55@gmail.com>
2016-09-17 16:52:59 +1000

    Find the global object (window) is quite persistent

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+/**
+ * appends the string 'World' to the value of `this`
+ * @param  {String} str
+ * @return {String}
+ */
+// this works, but I had to change the function this to this.str...:
+ var str = "Hello ";
+ function appendWorldToThis(){
+    return this + 'World';
+}
+console.log(appendWorldToThis(str));
+
+// var obj = { bar : function() {
+//                     var x = (() => this);
+//                     return x;
+//                   }
+//           };
+// tried some things.  ^ 'arrow function syntax (=>)' is only available in ES6 (use esnext option).
